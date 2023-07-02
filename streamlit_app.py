4da3d41206a4f31e69184b1e388dcdd18156a71c
CedrickJohannes <138369942+CedrickJohannes@users.noreply.github.com>
2023-07-03 00:11:16 +0200

    Update streamlit_app.py
    Add import snowflake.connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 import pandas
 
 
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -35,3 +34,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
